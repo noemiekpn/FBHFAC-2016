30801a69b2c032f98fae972db25c43d6be75b534
BrandonArroyo <barroyo50@gmail.com>
2016-06-18 04:41:35 -0700

    hmm

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,7 +4,7 @@ chrome.extension.sendMessage({}, function(response) {
 		clearInterval(readyStateCheckInterval);
 		var para = document.createElement("div");
 		para.innerHTML = '<div class="_5n2b _36bx _4-u2  _4-u8">\
-								<div class="_3-s"><div class="_6a _6b uiPopover" id="u_0_e"><a class="_42ft _42fu _4-s1 _2agf _p _42gx" role="button" href="#" style="max-width:200px;" aria-haspopup="true" aria-expanded="false" rel="toggle" id="u_0_g"><span class="_55pe" style="max-width:186px;"><i class="mrs img sp_mnsLGBPzuoP_2x sx_49614f"></i>Notifications</span><i class="_3-99 img sp_843e1e_2x sx_cd769d"></i></a></div></div>\
+								<div class="_3-s"><div class="_6a _6b uiPopover" id="u_0_e"><a class="_42ft _42fu _4-s1 _2agf _p _42gx" ><span class="_55pe" style="max-width:186px;"><i class="mrs img sp_mnsLGBPzuoP_2x sx_49614f"></i>Notifications</span><i class="_3-99 img sp_843e1e_2x sx_cd769d"></i></a></div></div>\
 								<div class="_4fhy _52jv">\
 									<img src="http://orig15.deviantart.net/9b9e/f/2013/185/6/8/682eaa32bcb514ef094d9bc2afd15cbc-d6bzng9.png" style="width:50px;height:50px;">\
 									<div class="_4fhz">Nepal Earthquake, April 25</div>\
