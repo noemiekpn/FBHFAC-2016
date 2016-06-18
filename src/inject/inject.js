3cc10fbd5468f8f2171626f560b9f749953f2687
noemiekpn <noemiekpn@tamu.edu>
2016-06-18 07:41:21 -0700

    Fixed button alignment, created blurb columns

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -10,15 +10,27 @@ chrome.extension.sendMessage({}, function(response) {
 				<div class="_4fhy _52jv">\
 					<img src="http://i.imgur.com/voCsxZH.png" id="notification-icon">\
 					<div class="_4fhz event-title">Nepal Earthquake, April 25</div>\
-					<div class="_4fh- drop-subtitle">FACEBOOK BLOOD DROP</div>\
+					<div class="_4fh- event-subtitle">FACEBOOK BLOOD DROP</div>\
+					<div class="_4fhz">\
+						<div class="column_2">\
+							<div class="_4fhz event-title">Donate when it\'s most needed</div>\
+							<div class="_4fhz event-content">Take immediate action to aid victims of tragic events in your area.</div>\
+						</div>\
+						<div class="column_2">\
+							<div class="_4fhz event-title">Donate where it\'s most needed</div>\
+							<div class="_4fhz event-content">Know the best location to donate according to your blood type.</div>\
+						</div>\
+					</div>\
 				</div>\
 			</div>\
 			<div class="_5n2b _36bx _4-u2  _4-u8 event-container">\
-				<div class="event-content">\
+				<div class="_4fhy _52jv message-content">\
 					<div class="_4fhz">You appear to be in the area affected by Nepal Earthquake.</div>\
-					<div class="_4fhz">There is an urgent call for blood.</div>\
+					<div class="_4fhz">There is an urgent for blood donors.</div>\
 				</div>\
-				<button class="uibutton special">Find out how you can help.</button>\
+				<div class="_4fhy _52jv">\
+					<button class="uibutton special">Find out how you can help.</button>\
+				</div>\
 			</div>'
 		var element = document.getElementById("stream_pagelet");
 
