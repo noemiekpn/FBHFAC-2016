6e9e542f57cd3ded9faadcca05d485a525206fa8
noemiekpn <noemiekpn@tamu.edu>
2016-06-18 06:06:25 -0700

    "Completed notification div, added pop up button"

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,19 +1,24 @@
 chrome.extension.sendMessage({}, function(response) {
-	alert($("#body").length);
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 		var para = document.createElement("div");
-		para.innerHTML = '<div class="_5n2b _36bx _4-u2  _4-u8">\
-								<div class="_3-s"><div class="_6a _6b uiPopover" id="u_0_e"><a class="_42ft _42fu _4-s1 _2agf _p _42gx" ><span class="_55pe" style="max-width:186px;"><i class="mrs img sp_mnsLGBPzuoP_2x sx_49614f"></i>Notifications</span><i class="_3-99 img sp_843e1e_2x sx_cd769d"></i></a></div></div>\
-								<div class="_4fhy _52jv">\
-									<img src="../img/drop_icon.png" id="notification-icon">\
-									<div class="_4fhz">Nepal Earthquake, April 25</div>\
-									<div class="_4fh-">FACEBOOK SAFETY CHECK</div>\
-								</div>\
-								<div class="_3bor">\
-								</div>\
-						</div>'
+
+		para.innerHTML = '\
+			<div class="_5n2b _36bx _4-u2  _4-u8 event-container">\
+				<div class="_4fhy _52jv">\
+					<img src="http://i.imgur.com/voCsxZH.png" id="notification-icon">\
+					<div class="_4fhz event-title">Nepal Earthquake, April 25</div>\
+					<div class="_4fh- drop-subtitle">FACEBOOK BLOOD DROP</div>\
+				</div>\
+			</div>\
+			<div class="_5n2b _36bx _4-u2  _4-u8 event-container">\
+				<div class="event-content">\
+					<div class="_4fhz">You appear to be in the area affected by Nepal Earthquake.</div>\
+					<div class="_4fhz">There is an urgent call for blood.</div>\
+				</div>\
+				<button class="uibutton special">Find out how you can help.</button>\
+			</div>'
 		var element = document.getElementById("stream_pagelet");
 
 		element.parentNode.insertBefore(para,element);
