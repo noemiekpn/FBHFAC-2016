5adee3d0d63357e1642a33a761d36366dc9692b0
BrandonArroyo <barroyo50@gmail.com>
2016-06-18 00:49:25 -0700

    added injection

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,7 +2,11 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
+		var para = document.createElement("p");
+		var node = document.createTextNode("This is new-------------.");
+		para.appendChild(node);
+		var element = document.getElementById("viewport");
+		element.appendChild(para);
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
