3069903e748db4da57fe03f430556c37cce45716
BrandonArroyo <barroyo50@gmail.com>
2016-06-18 01:46:55 -0700

    inserting in proper place

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,10 +3,10 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 		var para = document.createElement("p");
-		var node = document.createTextNode("This is new-------------.");
+		var node = document.createTextNode("TAJAJA");
 		para.appendChild(node);
-		var element = document.getElementById("viewport");
-		element.appendChild(para);
+		var element = document.getElementById("contentArea");
+		element.parentNode.insertBefore(node,element);
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
